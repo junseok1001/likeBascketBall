db2559ff527d837c341bc4bb9a853c74780bff4d
junseok1001_labtop <oj080700@naver.com>
2025-12-31 16:54:08 +0900

    [준석] - 첫번째 commit - 이제 프로젝트 시작 - 기능구현 및 templete 작성 시작

---
+++ src/main/java/com/sourjelly/likebasketball/LikeBasketBallApplication.java
@@ -0,0 +1,13 @@
+package com.sourjelly.likebasketball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikeBasketBallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LikeBasketBallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sourjelly/likebasketball/LikeBasketBallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sourjelly.likebasketball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikeBasketBallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
